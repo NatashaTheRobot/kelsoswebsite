21680f737dc6d833a94373b408b0568bac5b0951
NatashaMurashev <nmurashev@gmail.com>
2012-11-09 19:36:33 -0800

    image now resizes as window resizes

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -3,11 +3,15 @@ $(function(){
 
     function checkWidth() {
         if ($window.width() < 765) {
-           $('.columns').css('margin-top', '20px');
-           $('.six').css('padding', '0px 15px')
+            $('.columns').css('margin-top', '20px');
+            $('.six').css('padding', '0px 15px');
+            $('img').css('max-width', '75%');
+            $('img').css('margin-left', '30px');
         } else {
-           $('.columns').css('margin-top', '130px');
-           $('.six').css('padding', '0')
+            $('.columns').css('margin-top', '130px');
+            $('.six').css('padding', '0');
+            $('img').css('max-width', '100%');
+            $('img').css('margin-left', '0');
         }
     }
 
