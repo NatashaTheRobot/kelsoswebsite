7baa3b53747801453e02d7033f81ef59e19e4517
NatashaMurashev <nmurashev@gmail.com>
2012-11-09 19:25:53 -0800

    added resizing on when window width is resized

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -1,3 +1,16 @@
 $(function(){
+    var $window = $(window)
 
+    function checkWidth() {
+        if ($window.width() < 765) {
+           $('.columns').css('margin-top', '20px');
+           $('.six').css('padding', '0px 15px')
+        } else {
+           $('.columns').css('margin-top', '130px');
+           $('.six').css('padding', '0')
+        }
+    }
+
+    checkWidth();
+    $(window).resize(checkWidth);
 })
